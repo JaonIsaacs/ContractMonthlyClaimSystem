5d09207159b393eed248a0fe27f25c538e89132c
JaonIsaacs <102227507+JaonIsaacs@users.noreply.github.com>
2024-08-28 18:33:22 +0200

    Add project files.

---
+++ ContractMonthlyClaimSystem/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContractMonthlyClaimSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitClaimButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to open a new window for submitting a claim
+            SubmitClaimWindow submitClaimWindow = new SubmitClaimWindow();
+            submitClaimWindow.Show();
+        }
+
+        private void ViewClaimButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Logic to open a new window for viewing a claim
+            ViewClaimWindow viewClaimWindow = new ViewClaimWindow();
+            viewClaimWindow.Show();
+        }
+    }
+}
